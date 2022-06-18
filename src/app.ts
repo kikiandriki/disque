b7567d98645342f7a802ab104e3ff5dd559f4924
Mateja Lasan <m.vuk.lasan@icloud.com>
2022-06-18 21:11:53 +0000

    Add membership middleware and list members route

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import cors from "cors"
 // Middleware imports.
 import { auth } from "@middlewares/auth"
 import { error } from "@middlewares/error"
+import { member } from "@middlewares/member"
 
 // Repository imports.
 import { users } from "@repositories/users"
@@ -26,12 +27,20 @@ app.options("*", (_req, res) => res.status(204).send())
 // Use the auth middleware.
 app.use(auth)
 
-// Use routes.
-app.get("/@member", async (req, res, next) => {
+// Use public routes.
+app.get("/@member", async (req, res) => {
   const userId = req.actor.id
   const user = await users.get(userId)
-  res.status(200).send(!!user)
-  return next()
+  return res.status(200).send(!!user)
+})
+
+// Use the membership middleware.
+app.use(member)
+
+// Use private routes.
+app.get("/members", async (_req, res) => {
+  const members = await users.getAll()
+  return res.status(200).send(members)
 })
 
 // Use the error middleware.
