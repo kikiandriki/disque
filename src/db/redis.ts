2ef69a89c0f15c8e5877825c01c2021afae51ab0
Mateja Lasan <m.vuk.lasan@icloud.com>
2022-06-24 17:17:16 +0000

    Update key prefix

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -7,5 +7,5 @@ import Redis from "ioredis"
 
 // The Redis client.
 export const redis = new Redis(process.env.REDIS_URL || "redis://localhost", {
-  keyPrefix: "ladder:",
+  keyPrefix: "disque:",
 })
